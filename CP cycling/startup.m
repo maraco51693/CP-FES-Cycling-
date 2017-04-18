cd88fc5a54b76df9dee09e86e863ca2d036a39d2
TedisAgolli <tagolli@udel.edu>
2017-04-18 12:37:39 -0400

    cadence control

--- CP cycling/startup.m
+++ CP cycling/startup.m
@@ -1,4 +1,7 @@
 clc;
-t = zeros(14819,1);
+
+time = transpose(0:0.05:14818*0.05);
+shaft_encoder_validity= zeros(14819,1);
+throttle_validity = zeros(14819,1);
 load FESCycling_30_PRE_CONST_loaded_12Aug2014_085817.mat;
 select_test;
