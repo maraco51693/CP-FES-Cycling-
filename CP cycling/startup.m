62e217e37db0fde19b27ecab491a8b49f1dd333c
TedisAgolli <tagolli@udel.edu>
2017-04-28 10:59:31 -0400

    '.'

--- CP cycling/startup.m
+++ CP cycling/startup.m
@@ -1,6 +1,7 @@
 clc;
 
-time = transpose(0:0.05:14818*0.05);
+time = transpose(0:1:14818*1);
+%t = ones(14819,1);
 shaft_encoder_validity= zeros(14819,1);
 throttle_validity = zeros(14819,1);
 load FESCycling_30_PRE_CONST_loaded_12Aug2014_085817.mat;
